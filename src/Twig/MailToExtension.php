d76b5499a72ed7f14d95dff856106e27d5b62a56
lev <levdanilin@gmail.com>
2021-09-01 19:01:44 +0200

    mailTo-changes

--- src/Twig/MailToExtension.php
+++ src/Twig/MailToExtension.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig;
 
+use App\DTO\MailTo;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -10,8 +11,39 @@ class MailToExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('mailToLinkGenerator', [MailToRuntime::class, 'generateLink']),
+            new TwigFunction('mailToLinkGenerator', [$this, 'generateLink']),
         ];
     }
 
+    public function generateLink(MailTo $mailToObject): string
+    {
+        $mailToLink = 'mailto:' . $mailToObject->getAddress();
+        $mailToParameters = [];
+
+        if($mailToObject->getSubject() !== null)
+        {
+            $mailToParameters['subject'] = $mailToObject->getSubject();
+        }
+
+        if($mailToObject->getCc() !== null)
+        {
+            $mailToParameters['cc'] = $mailToObject->getCc();
+        }
+
+        if($mailToObject->getBcc() !== null)
+        {
+            $mailToParameters['bcc'] = $mailToObject->getBcc();
+        }
+
+        if($mailToObject->getBody() !== null)
+        {
+            $mailToParameters['body'] = $mailToObject->getBody();
+        }
+
+        if(count($mailToParameters) > 0)
+        {
+            $mailToLink .= '?' . http_build_query($mailToParameters);
+        }
+        return $mailToLink;
+    }
 }
